3b7f9f6eebad8ab712780934e72d5890bb2348b0
Ish Thumber <80597098+IshThumber@users.noreply.github.com>
2022-01-13 14:19:39 +0530

    Complete

--- Assign1/dictionary.py
+++ Assign1/dictionary.py
@@ -1,31 +1,31 @@
 ## a.
-# dic = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5}
-# key1 = 'A'
-# key2 = 'G'
-# if key1 in dic:
-#     print(dic[key1], "is present in the dictionary")
-# else:
-#     print("Key is not present in the dictionary")
-# if key2 in dic:
-#     print(dic[key2], "is present in the dictionary")
-# else:
-#     print("Key is not present in the dictionary")
+dic = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5}
+key1 = 'A'
+key2 = 'G'
+if key1 in dic:
+    print(dic[key1], "is present in the dictionary")
+else:
+    print("Key is not present in the dictionary")
+if key2 in dic:
+    print(dic[key2], "is present in the dictionary")
+else:
+    print("Key is not present in the dictionary")
 
 ## b.
-# dic1 = {'a': 100, 'b': 200}
-# dic2 = {'x': 300, 'y': 200}
-# dic = dic1.copy()
-# dic.update(dic2)
-# print(dic)
+dic1 = {'a': 100, 'b': 200}
+dic2 = {'x': 300, 'y': 200}
+dic = dic1.copy()
+dic.update(dic2)
+print(dic)
 
 ## c.
-# dic = {'a': 100, 'b': 200}
-# print("Total Sum of values in the dictionary:", sum(dic.values()))
+dic = {'a': 100, 'b': 200}
+print("Total Sum of values in the dictionary:", sum(dic.values()))
 
 ## d.
-# dic = {'a': 100, 'b': 200}
-# dic.update({'c': 300})
-# print(dic)
+dic = {'a': 100, 'b': 200}
+dic.update({'c': 300})
+print(dic)
 
 ## e.
 dic1={1:10, 2:20}
