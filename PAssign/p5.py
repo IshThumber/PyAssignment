8383d5bedb3fe9f1cd75fe21b84a5890c97355a6
Ish Thumber <80597098+IshThumber@users.noreply.github.com>
2022-04-24 19:17:06 +0530

    Completed

--- PAssign/p5.py
+++ PAssign/p5.py
@@ -1,41 +1,42 @@
+import sys
+while True:
+    try:
+        number = int(input("Enter a number: "))
+        break
+    except ValueError:
+        print("That's not a valid number! Try again.")
+
+print("Your number doubled is:", number * 2)
+
+default_greeting = "Hello World!"
+filename = "greeting.txt"
+
+
+def askyesno(question):
+    while True:
+        answer = input(question + ' (y or n) ')
+        if answer == 'Y' or answer == 'y':
+            return True
+        if answer == 'N' or answer == 'n':
+            return False
+
+
+def greet():
+    with open(filename, 'r') as f:
+        for line in f:
+            print(line.rstrip('\n'))
+
+
 try:
-    a = 10 / 0
-    print(a)
-except ArithmeticError:
-    print("This statement is raising an arithmetic exception.")
-else:
-    print("Success.")
+    greet()
+except OSError:
+    print("Cannot read '%s'!" % filename, file=sys.stderr)
+    if askyesno("Would you like to create a default greeting file?"):
+        with open(filename, 'w') as f:
+            print(default_greeting, file=f)
+        greet()
 
 
-#####################
 
 
-try:
-    a = [1, 2, 3]
-    print(a[3])
-except LookupError:
-    print("Index out of bound error.")
 
-
-####################
-
-
-class Attributes(object):
-    pass
-
-
-object = Attributes()
-print(object.attribute)
-
-#####################
-
-import math
-
-print(math.exp(1000))
-
-
-#####################
-
-
-array = [0, 1, 2]
-print(array[3])
